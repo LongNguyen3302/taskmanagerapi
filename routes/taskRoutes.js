a3f6917f5b1f154c50c19c083ec33cc0263ca43b
khoi <tran5325@saskpolytech.ca>
2024-11-25 16:50:19 -0600

    complete

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const { createTask, getTasks, getTaskById, updateTask, deleteTask } = require('../controllers/taskC');
-const { authenticate } = require('../middleware/authMiddleware'); // Import your authentication middleware
+const { authenticate } = require('../middleware/authMiddleware');
+const authMiddleware = require("../middleware/authMiddleware"); // Import your authentication middleware
 const router = express.Router();
 
 
 // Define the routes
 router.post('/create', createTask);        // Create a new task
 router.get('/gettask', getTasks);           // Get all tasks
-router.put('/update', updateTask);     // Update a task by ID
-router.delete('/delete', deleteTask);  // Delete a task by ID
+router.put('/:id', authMiddleware('read'), updateTask);     // Update a task by ID
+router.delete('/:id', authMiddleware('read'), deleteTask);  // Delete a task by ID
 
 module.exports = router;
